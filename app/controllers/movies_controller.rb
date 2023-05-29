6bec1e237cb7550a5e5b37f14958f471fde9663f
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-29 12:17:37 +0300

    Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,10 +6,29 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
-  def show
+    def show
+    #METHOD 1
+    # movie = Movie.find(params[:id])
+    # render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+
+    #METHOD 2
+    # movie = Movie.find(params[:id])
+    # render json: movie.to_json(except: [:created_at, :updated_at])
+
+    #METHOD 3 - Ideal AMS Serializer
     movie = Movie.find(params[:id])
     render json: movie
-  end
+   end
+
+   def summary
+      movie = Movie.find(params[:id])
+      render json: movie, serializer: MovieSummarySerializer
+   end
+
+   def summaries
+     movies = Movie.all
+     render json: movies, each_serializer: MovieSummarySerializer
+   end
 
   private
 
